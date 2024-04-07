4a8b70cd4eb36307bb2bcd52f7c5e8f65e73f4d0
Aditya Bhatnagar <bhatanagar88.aditya@gmail.com>
2024-04-07 12:56:48 +0530

    configure chat app relay host to read from properties file

--- ChatApp/src/main/java/com/app/chatapp/config/WebSocketConfig.java
+++ ChatApp/src/main/java/com/app/chatapp/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.app.chatapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${stomp.relay.host}")
+    private String relayHost;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
@@ -17,6 +21,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableStompBrokerRelay("/topic").setSystemHeartbeatSendInterval(1000).setSystemHeartbeatReceiveInterval(1000);
+        registry.enableStompBrokerRelay("/topic").setRelayHost(relayHost).setSystemHeartbeatSendInterval(1000).setSystemHeartbeatReceiveInterval(1000);
     }
 }
