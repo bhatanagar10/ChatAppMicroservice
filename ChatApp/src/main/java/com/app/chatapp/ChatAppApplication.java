2f4976d4a882eed8633ab445b60cf02de3e1189f
Aditya Bhatnagar <bhatanagar88.aditya@gmail.com>
2024-04-07 12:15:50 +0530

    chatapp externalize configuration & application-docker.yml

--- ChatApp/src/main/java/com/app/chatapp/ChatAppApplication.java
+++ ChatApp/src/main/java/com/app/chatapp/ChatAppApplication.java
@@ -2,13 +2,29 @@ package com.app.chatapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ChatAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatAppApplication.class, args);
+//        SpringApplication.run(ChatAppApplication.class, args);
+
+        new SpringApplicationBuilder(ChatAppApplication.class).sources(ChatAppApplication.class).properties(getProperties())
+                .run(args);
+    }
+
+    static Properties getProperties() {
+        Properties props = new Properties();
+        String externalConfigDir = "file:///chatapp/";
+        String applicationYml = externalConfigDir + "application.yml";
+        String applicationDockerYml = externalConfigDir + "application-docker.yml";
+
+        props.put("spring.config.location", applicationYml + "," + applicationDockerYml);
+//        props.put("spring.config.location", "classpath:application.yml");
+        return props;
     }
 }
