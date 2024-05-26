1882376ac7003cf8acdedfb52c5abd202dac63b2
Aditya Bhatnagar <bhatanagar88.aditya@gmail.com>
2024-05-26 12:09:10 +0530

    add stop relay host in application.yml - disable externalize properties in ChatAppApplication - comment volumes for chatapp in docker-compose.yaml

--- ChatApp/src/main/java/com/app/chatapp/ChatAppApplication.java
+++ ChatApp/src/main/java/com/app/chatapp/ChatAppApplication.java
@@ -11,20 +11,19 @@ import java.util.Properties;
 public class ChatAppApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(ChatAppApplication.class, args);
-
-        new SpringApplicationBuilder(ChatAppApplication.class).sources(ChatAppApplication.class).properties(getProperties())
-                .run(args);
+        SpringApplication.run(ChatAppApplication.class, args);
+      /*  new SpringApplicationBuilder(ChatAppApplication.class).sources(ChatAppApplication.class).properties(getProperties())
+                .run(args);*/
     }
 
-    static Properties getProperties() {
+/*    static Properties getProperties() {
         Properties props = new Properties();
         String externalConfigDir = "file:///chatapp/";
         String applicationYml = externalConfigDir + "application.yml";
         String applicationDockerYml = externalConfigDir + "application-docker.yml";
 
-        props.put("spring.config.location", applicationYml + "," + applicationDockerYml);
-//        props.put("spring.config.location", "classpath:application.yml");
+        props.put("spring.config.location", applicationYml + "," + applicationDockerYml);*//*
+        props.put("spring.config.location", "classpath:application.yml");
         return props;
-    }
+    }*/
 }
